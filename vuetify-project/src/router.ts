72408e089f09e6790805a3fbab54d269f964e6d4
Kumbirai Shonhiwa <161568374+KumbiraiShonhiwa@users.noreply.github.com>
2024-05-18 13:21:22 +0200

    Created project

---
+++ vuetify-project/src/router.ts
@@ -0,0 +1,8 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./App.vue";
+import 'vuetify/dist/vuetify.min.css';
+import Vuetify from 'vuetify';
+
+
+Vue.use(VueRouter);
